2c9962ad6f69c99f4dc8b2df9928019fb9144ab1
Gabriel Chaves de Oliveira Sales <gabriel.oliveira@natto-br.com>
2025-05-22 16:08:26 -0300

    Atualização

--- Connects/script.js
+++ Connects/script.js
@@ -2,15 +2,20 @@ $(document).ready(function() {
     function toggleTheme() {
         const body = $('body');
         const button = $('.theme-toggle');
+        const cards = $('.project-card');
         
         if (body.hasClass('light-mode')) {
             body.removeClass('light-mode').addClass('dark-mode');
+            cards.removeClass('project-card').addClass('project-card-dark')
             button.text('Modo Claro');
         } else {
             body.removeClass('dark-mode').addClass('light-mode');
+            cards.removeClass('project-card-dark').addClass('project-card')
             button.text('Modo Noturno');
         }
     }
+
+
 
     $('.theme-toggle').on('click', toggleTheme);
 
@@ -33,4 +38,10 @@ $(document).ready(function() {
 
 document.getElementById("menu-toggle").addEventListener("click", function () {
     document.getElementById("menu-nav").classList.toggle("active");
-});  
+}); 
+
+function scrollProjects(direction) {
+    const container = document.getElementById('projects');
+    const scrollAmount = 300;
+    container.scrollBy({ left: direction * scrollAmount, behavior: 'smooth' });
+}
